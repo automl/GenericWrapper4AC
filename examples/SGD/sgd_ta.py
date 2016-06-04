517bce306b530e1598293f7c8d30d09097c08752
mlindauer <lindauer@informatik.uni-freiburg.de>
2016-06-04 18:57:08 +0200

    FIX SGD split

--- examples/SGD/sgd_ta.py
+++ examples/SGD/sgd_ta.py
@@ -6,8 +6,8 @@ from sklearn import cross_validation
 
 iris = load_iris()
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(iris.data, iris.target, test_size=0.25, random_state=0)
-X_train, X_valid, y_train, y_valid = cross_validation.train_test_split(X_train, y_train, test_size=0.25, random_state=0)
+X_train_f, X_test, y_train_f, y_test = cross_validation.train_test_split(iris.data, iris.target, test_size=0.25, random_state=0)
+X_train, X_valid, y_train, y_valid = cross_validation.train_test_split(X_train_f, y_train_f, test_size=0.25, random_state=0)
 
 params = iter(sys.argv[2:])
 
@@ -45,11 +45,11 @@ sgd = SGDClassifier(random_state=random_state,
     
 #print(sgd.loss, sgd.penalty, sgd.alpha, sgd.learning_rate, sgd.eta0)
         
-sgd.fit(X_train,y_train)
-
 if sys.argv[1] == "train":
+    sgd.fit(X_train,y_train)
     print(-1 * sgd.score(X_valid, y_valid))
 else:
+    sgd.fit(X_train_f,y_train_f)
     print(-1 * sgd.score(X_test, y_test))
 
 
